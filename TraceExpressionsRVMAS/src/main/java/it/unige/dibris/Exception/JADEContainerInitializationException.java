2d1eef3b25c97dd40289d35e4cc4a8bd8aaf4aa8
angeloferrando <angelo.ferrando42@gmail.com>
2017-05-23 11:29:14 +0200

    JADEAgentInitializationException class added. JADEContainerInitializationException class added.

---
+++ TraceExpressionsRVMAS/src/main/java/it/unige/dibris/Exception/JADEContainerInitializationException.java
@@ -0,0 +1,30 @@
+package it.unige.dibris.Exception;
+
+public class JADEContainerInitializationException extends RuntimeException {
+
+	public JADEContainerInitializationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public JADEContainerInitializationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JADEContainerInitializationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JADEContainerInitializationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JADEContainerInitializationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
