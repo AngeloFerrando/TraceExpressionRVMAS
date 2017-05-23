0de313bf7351c008309336c0439bf725f8155866
angeloferrando <angelo.ferrando42@gmail.com>
2017-05-23 11:34:19 +0200

    Integration of the sniffer agent inside the Boot class (only the centralized version). Tested with the alt_bit example.

--- TraceExpressionsRVMAS/src/main/java/it/unige/dibris/TExpRVMAS/Boot.java
+++ TraceExpressionsRVMAS/src/main/java/it/unige/dibris/TExpRVMAS/Boot.java
@@ -1,10 +1,25 @@
 package it.unige.dibris.TExpRVMAS;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import it.unige.dibris.Exception.EnvironmentVariableNotDefined;
+import it.unige.dibris.Exception.JADEAgentInitializationException;
+import it.unige.dibris.Exception.JADEContainerInitializationException;
 import it.unige.dibris.Exception.JavaLibraryPathException;
+import it.unige.dibris.monitor.Sniffer;
+import it.unige.dibris.utils.JPL.JPLInitializer;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
+import jade.wrapper.StaleProxyException;
 
 /**
  * Boots the TraceExpressionsRVMAS system, parsing command line arguments.
@@ -21,13 +36,25 @@ public class Boot {
 	 * @param args 
 	 * trace expression file containing the protocol to verify, and a list of JADE agents to execute/monitor 
 	 * <path_to_trace_expression_file> <jade-agent1> ... <jade-agentN>
-	 * @throws Exception
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
-		String swipl = System.getenv("SWI_PROLOG_HOME");
+	public static void main(String[] args) throws FileNotFoundException {
 		
+		args = new String[2];
+		args[0] = "/Users/angeloferrando/Desktop/trace_expression.texp";
+		args[1] = "alice:alt_bit.Sender(bob,msg1,5000);bob:alt_bit.Receiver(alice,ack1);charlie:alt_bit.Sender(david,msg2,10000);david:alt_bit.Receiver(charlie,ack2)";
+		
+		/* The arguments must be at least 2, the trace expression file and one (or more) agent(s) */
+		if(args.length != 2){
+			throw new IllegalArgumentException("Too few arguments: expected <path_to_trace_expression_file> <jade-agent1>;...;<jade-agentN>");
+		}
+		
+		/* Retrieve the SWI_LIB environment variable */
+		String swipl = System.getenv("SWI_LIB");
+		
+		/* If it does not exist an exception is thrown */
 		if(swipl == null){
-			throw new EnvironmentVariableNotDefined("SWI_PROLOG_HOME environment variable not defined");
+			throw new EnvironmentVariableNotDefined("SWI_LIB environment variable not defined");
 		}
 		
 		/* We need to add the SWI-Prolog Home to the path in order to use the JPL library */
@@ -37,6 +64,66 @@ public class Boot {
 			throw new JavaLibraryPathException("An error occured during the user path retrieval information process", e);
 		}
 		
+		/* Now we check only if the file exists. 
+		 * TO-DO To check if the file is syntactically and semantically correct */
+		String tExpPath = args[0];
+		File tExpFile = new File(tExpPath);
+		if(!tExpFile.exists()){ 
+		    throw new FileNotFoundException(tExpPath + " file not found");
+		}
+		
+		/* SWI-Prolog environment initialization (transition system, DecAMon, current trace expression) */
+		JPLInitializer.init(tExpPath);
+		
+		/* Initialize JADE environment */
+		jade.core.Runtime runtime = jade.core.Runtime.instance();
+		Profile profile = new ProfileImpl();
+		AgentContainer container = runtime.createMainContainer( profile );
+		
+		/* List of JADE agents */
+		List<AgentController> agents = new ArrayList<>();
+		
+		/* Parsing JADE agents */
+		String[] agentsargs = args[1].split(";");
+		/* Partition used for the runtime verification */
+		String projectionSet = "[";
+		for(String agent : agentsargs){
+			String name = agent.split(":")[0];
+			projectionSet += name + ",";
+			String constructor = agent.split(":")[1];
+			if(!constructor.contains("(") || !constructor.contains(")")){
+				throw new IllegalArgumentException("JADE agents must follow the syntax: name:type(arg1,...,argN)");
+			}
+			String type = constructor.split("\\(")[0];
+			Object[] arguments = constructor.split("\\(")[1].replace(")", "").split(",");
+			try {
+				agents.add(container.createNewAgent(name, type, arguments));
+			} catch (StaleProxyException e) {
+				throw new JADEAgentInitializationException("Unable to create the agent " + name + " of type " + type, e);
+			}
+		}
+		projectionSet = projectionSet.substring(0, projectionSet.length()-1) + "]";
+		/* Sniffer creation */
+		Sniffer s = new Sniffer();
+		s.setArguments(new String[]{
+				"snifferCentralized.txt",
+				projectionSet
+		});
+		try{
+			AgentController ac = container.acceptNewAgent("snifferCentralized", s);
+			ac.start();
+			for(AgentController agent : agents){
+				agent.start();
+			}
+		} catch(StaleProxyException e){
+			throw new JADEContainerInitializationException("Unable to start an agent container", e);
+		}
+		
+		/* Set to close the JVM when JADE environment ends */
+		jade.core.Runtime.instance().setCloseVM(true);		
+	}
+	
+	public static void executeMonitors(Partition p){
 		
 	}
 	
