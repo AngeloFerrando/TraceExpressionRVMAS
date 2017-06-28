213dd798360c8dbedef408d24aa6d6cdcd1b74a0
angeloferrando <angelo.ferrando42@gmail.com>
2017-06-28 10:27:47 +0200

    are_all_events_atomic and are_all_events_async updated.

--- TraceExpressionsRVMAS/src/main/java/it/unige/dibris/TExpRVMAS/core/protocol/TraceExpression.java
+++ TraceExpressionsRVMAS/src/main/java/it/unige/dibris/TExpRVMAS/core/protocol/TraceExpression.java
@@ -515,9 +515,9 @@ public class TraceExpression {
 	 * @return true if all events are async, false otherwise
 	 */
 	public boolean areEventsAsync() {
-		Query query1 = new Query("are_all_events_atomic_aux(" + protocolName + ")");
+		Query query1 = new Query("are_all_events_atomic(" + protocolName + ")");
 		boolean res1 = query1.hasSolution();
-		Query query2 = new Query("are_all_events_async_aux(" + protocolName + ")");
+		Query query2 = new Query("are_all_events_async(" + protocolName + ")");
 		boolean res2 = query2.hasSolution();
 		query1.close();
 		query2.close();
